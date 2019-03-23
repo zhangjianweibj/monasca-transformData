14b891e955c21616aa6666dba620aa1985b08b93
zhangjianweibj <zhangjianweibj@inspur.com>
2019-03-23 09:54:59 +0800

    confluent version modify

--- vendor/github.com/zhangjianweibj/monasca-transformData/models/metric.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Metric struct {
-	Name       string            `json:"name"`
-	Dimensions map[string]string `json:"dimensions"`
-	Timestamp  float64           `json:"timestamp"`
-	Value      float64           `json:"value"`
-	ValueMeta  map[string]string `json:"value_meta"`
-}
--- vendor/github.com/zhangjianweibj/monasca-transformData/models/metric_envelope.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type MetricEnvelope struct {
-	Metric       Metric            `json:"metric"`
-	Meta         map[string]string `json:"meta"`
-	CreationTime int64             `json:"creation_time"`
-}
